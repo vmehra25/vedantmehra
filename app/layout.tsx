c99566ce986d2ab84a8ea8e260824e814c65e234
VEDANT MEHRA <vedantmehra25@gmail.com>
2024-07-13 15:14:08 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+
+import { siteConfig } from "@/config/site";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playwrite+PE:wght@100..400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@nextui-org/button";
+import { Divider } from "@nextui-org/divider";
+import { Link } from "@nextui-org/link";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-50 p-4 h-screen">
+      <section>
+        <div className="flex items-center justify-between">
+          <Link
+            isBlock
+            href="/"
+            className="text-blue-950 lg:text-7xl md:text-6xl text-5xl font-bold mb-4 transition"
+          >
+            Vedant Mehra
+          </Link>
+          <Link isExternal href="https://www.linkedin.com/in/vmehra25/">
+            <Button
+              radius="full"
+              className="bg-red-500 text-white"
+              target="_blank"
+            >
+              Contact
+            </Button>
+          </Link>
+        </div>
+      </section>
+      <Divider></Divider>
+      <section className="pt-4">
+        <p className="text-center lg:text-5xl text-4xl text-orange-500 ">
+          Intriguing Insights: From Text & The World
+        </p>
+      </section>
+      <section className="mt-16 lg:px-32 md:px-16 px-8">
+        <p className="text-3xl text-stone-500 mb-4">July 2024</p>
+        <Link
+          isBlock
+          showAnchorIcon
+          href="https://www.youtube.com/watch?v=e0WZx7lUOrY&list=PL848F2368C90DDC3D"
+          color="foreground"
+          target="_blank"
+          className="text-cyan-600 mr-8"
+        >
+          Behavioral Genetics I
+        </Link>
+        <Link
+          isBlock
+          showAnchorIcon
+          href="https://read.amazon.in/kp/embed?asin=B09QHBZDDZ&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_5H184PK7GQJRHM95G7K3"
+          color="foreground"
+          target="_blank"
+          className="text-red-600"
+        >
+          Amp It Up
+        </Link>
+      </section>
+    </div>
+  );
+}
