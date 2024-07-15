8bf9b7848b50ecfc29bfdac79dfc54f8f8164a06
VEDANT MEHRA <vedantmehra25@gmail.com>
2024-07-15 23:50:15 +0530

    updated website descrpition

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Vedant Mehra",
-  description: "Make beautiful websites regardless of your design experience.",
+  description: "Hi, I'm Vedant Mehra, this is a peek to my interests.",
   navItems: [
     {
       label: "Home",
